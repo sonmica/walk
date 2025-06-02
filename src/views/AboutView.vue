c1176c052bad948820a8e500d73816d5eef810f7
Deacon McIntyre <gellelao@gmail.com>
2025-06-02 14:01:14 +1200

    Progress bar

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,18 +1,13 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
-    <div class="alert alert-primary" role="alert">
-      A simple primary alert—check it out!
+    <div class="progress" role="progressbar" aria-label="Example with label" aria-valuenow="100" aria-valuemin="0" aria-valuemax="789" style="height: 20px">
+      <div class="progress-bar" style="width: 70%"></div>
     </div>
   </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+  
 }
 </style>
