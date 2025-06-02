8f7e19e9b68142df1f30eec9ebbd730f4209aeb8
Deacon McIntyre <gellelao@gmail.com>
2025-06-02 16:17:39 +1200

    Remove bar from About page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -6,17 +6,6 @@
         You can use this app to track your daily steps, and join your friends in themed challenges.
       </div>
     </div>
-    <div
-      class="progress"
-      role="progressbar"
-      aria-label="Example with label"
-      aria-valuenow="100"
-      aria-valuemin="0"
-      aria-valuemax="789"
-      style="height: 20px"
-    >
-      <div class="progress-bar" style="width: 70%"></div>
-    </div>
   </div>
 </template>
 
