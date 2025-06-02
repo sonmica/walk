ef8b53b222f39d8c8cc868a6127c957068a89b38
tech.ana <technically.ana@proton.me>
2025-06-02 14:16:37 +1200

    added login

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import DashboardView from '@/views/DashboardView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,6 +23,7 @@ const router = createRouter({
       name: 'challenges',
       component: () => import('../views/Challenges.vue'),
     },
+    { path: '/dashboard/:id', name: 'dashboard', component: DashboardView},
   ],
 })
 
