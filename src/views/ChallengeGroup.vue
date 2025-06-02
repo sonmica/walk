2cf437e0f15f603c6a2e8291459f5e0247e88760
Mary Tionko <mary.tionko@gmail.com>
2025-06-02 16:46:21 +1200

    Adds mock data to Challenge group page

--- src/views/ChallengeGroup.vue
+++ src/views/ChallengeGroup.vue
@@ -1,8 +1,26 @@
 <template>
   <div class="challengeGroup">
-    Challenges Page (group)
+    <h2>Challenge Group</h2>
+    <h5>Walk the Shire</h5>
+
+    <div class="pb-2">
+      <h6>Leaderboard</h6>
+      <ol class="list-group list-group-numbered" data-bs-theme="dark">
+        <li class="list-group-item">Barry Boring</li>
+        <li class="list-group-item">Penelope Peculiar</li>
+        <li class="list-group-item">Bruce Wayne</li>
+        <li class="list-group-item list-group-item-primary">Marty McFly</li>
+        <li class="list-group-item">Diana Disaster</li>
+        <li class="list-group-item">Peter Perfect</li>
+      </ol>
+    </div>
+    <div>
+      <h6>You</h6>
+      <ul class="list-group" data-bs-theme="dark">
+        <li class="list-group-item list-group-item-primary"><span>4. </span>Marty McFly</li>
+      </ul>
+    </div>
   </div>
 </template>
 
-<style>
-</style>
+<style></style>
