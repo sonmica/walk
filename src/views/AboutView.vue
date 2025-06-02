92b117c5e0b195f167c72406db026d2fbf8216e1
Deacon McIntyre <gellelao@gmail.com>
2025-06-02 12:52:08 +1200

    CDN Bootstrap

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div class="alert alert-primary" role="alert">
+      A simple primary alert—check it out!
+    </div>
   </div>
 </template>
 
